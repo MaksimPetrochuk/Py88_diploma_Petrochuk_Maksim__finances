503046828e7e70e2f2b7f93f440bc16e1838f1f2
MaksimPetrochuk <max.96y@gmail.com>
2023-11-01 19:56:40 +0300

    Images, error list, requirements

--- finances/consumer/forms.py
+++ finances/consumer/forms.py
@@ -3,27 +3,6 @@ from django.contrib.auth.forms import UserCreationForm
 from consumer.models import Consumer
 
 
-class ConsumerSignUpForm(UserCreationForm):
-    email = EmailField(help_text='A valid email', required=True)
-    class Meta:
-        model = Consumer
-        fields = ['username', 'email', 'password']
-
-    def save(self, commit=True):
-        user = super(ConsumerSignUpForm, self).save(commit=False)
-        user.email = self.clean_data['email']
-        if commit:
-            user.save()
-
-        return user
-
-
-class ConsumerSignInForm(ModelForm):
-    class Meta:
-        model = Consumer
-        fields = ['email', 'password']
-
-
 class RegistrationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = Consumer
